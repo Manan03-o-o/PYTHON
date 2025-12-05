cbb40417381b38aec536d3f2d9f0ac50b813c943
Manan <manannagpal003@gmail.com>
2025-12-05 22:22:05 +0530

    created To find prime no in range

---
+++ Chapter_01/PrimeNo.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+a = int(input("Start: "))
+b = int(input("End: "))
+
+for i in range(a, b+1):
+    if is_prime(i):
+        print(i, end=" ")
