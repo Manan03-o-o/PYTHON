af19d39ab566c22126f8b8f9460d20930fac896a
Manan <manannagpal003@gmail.com>
2025-12-06 21:32:44 +0530

    created reverse words in a scentnce2

--- Chapter_02/reverse.py
+++ Chapter_02/reverse.py
@@ -4,3 +4,12 @@ words = sentence.split()
 rev = " ".join(words[::-1])
 
 print("Reversed:", rev)
+print("Original:", sentence)
+print("Original id:", id(sentence))
+print("Reversed id:", id(rev))
+
+print
+("Are they the same object?", sentence is rev)
+print("Are they equal?", sentence == rev)
+
+print("Original length:", len(sentence))
