2bf2aef95613f239724ada61bd857cde1674df75
Manan <manannagpal003@gmail.com>
2025-12-07 20:47:32 +0530

    created find second largest no. from the list theroy

--- Chapter_03/findno.py
+++ Chapter_03/findno.py
@@ -10,3 +10,16 @@ for num in arr:
         second = num
 
 print("Second Largest =", second)
+# Find the second largest number in a list of numbers provided by the user.
+# If there is no second largest number, it will print -inf.
+# Example Input: 10 20 4 45 99
+# Example Output: Second Largest = 45
+# Example Input: 10 10 10
+# Example Output: Second Largest = -inf
+
+# Example Input: 5 3 8 6 2
+# Example Output: Second Largest = 6
+#
+# Example Input: 1
+# Example Output: Second Largest = -inf
+
