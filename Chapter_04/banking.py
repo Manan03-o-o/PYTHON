cc5f04a0ffac9d416b2ac5eab75cb1458a8dafd8
Manan <manannagpal003@gmail.com>
2025-12-08 10:19:32 +0530

    created mini banking system

---
+++ Chapter_04/banking.py
@@ -0,0 +1,31 @@
+accounts = {
+    "manan": 2000,
+    "raj": 1500,
+    "tara": 3000
+}
+
+user = input("Enter username: ")
+
+if user in accounts:
+    while True:
+        print("\n1.Deposit\n2.Withdraw\n3.Balance\n4.Exit")
+        ch = int(input("Choice: "))
+
+        if ch == 1:
+            amt = int(input("Amount: "))
+            accounts[user] += amt
+
+        elif ch == 2:
+            amt = int(input("Amount: "))
+            if amt <= accounts[user]:
+                accounts[user] -= amt
+            else:
+                print("Low balance!")
+
+        elif ch == 3:
+            print("Balance =", accounts[user])
+
+        else:
+            break
+else:
+    print("User not found!")
