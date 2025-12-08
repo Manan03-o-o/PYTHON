4e6d2cf0bd4a9324db2e1a755a683d834ca2189d
Manan <manannagpal003@gmail.com>
2025-12-08 10:24:03 +0530

    created resturant billing system theory

--- Chapter_04/billing.py
+++ Chapter_04/billing.py
@@ -14,3 +14,9 @@ while True:
         print("Item not available")
 
 print("Total bill =", bill)
+# This code allows users to order items from a menu and calculates the total bill.
+# Users can order multiple items until they choose to exit.
+# The menu is represented as a dictionary with item names as keys and prices as values.
+# The total bill is updated based on the items ordered and their quantities.
+# If an item is not available in the menu, an error message is displayed.
+
