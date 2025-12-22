b4d7bc8c336cf2848a58330b3383d9e808cf0b2b
Manan <manannagpal003@gmail.com>
2025-12-22 21:05:07 +0530

    created wheather system op

--- Chapter_07/wheather.py
+++ Chapter_07/wheather.py
@@ -6,3 +6,9 @@ weather = json.loads(data)
 print("City:", weather["city"])
 print("Temp:", weather["temp"])
 print("Humidity:", weather["humidity"])
+# Output:
+# City: Delhi
+# Temp: 30
+# Humidity: 60
+# The code above demonstrates how to parse a JSON string in Python using the json module.
+
