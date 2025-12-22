64c443a319cc5f1b5e8604796d0001687f1e372b
Manan <manannagpal003@gmail.com>
2025-12-22 21:01:22 +0530

    created employee payroll

---
+++ Chapter_07/employeepayroll.py
@@ -0,0 +1,13 @@
+class Employee:
+    def __init__(self, emp_id, name, salary):
+        self.emp_id = emp_id
+        self.name = name
+        self.salary = salary
+
+    def calculate_salary(self):
+        hra = self.salary * 0.20
+        da = self.salary * 0.10
+        return self.salary + hra + da
+
+e = Employee(101, "Riya", 30000)
+print("Total Salary:", e.calculate_salary())
