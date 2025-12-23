6f3e50b064070ff444d41f8607d6dda448bcb52f
Manan <manannagpal003@gmail.com>
2025-12-23 20:34:27 +0530

    created hospital managment system class patient

--- Chapter_08/hospital.py
+++ Chapter_08/hospital.py
@@ -19,3 +19,24 @@ while True:
             print(p.pid, p.name, p.disease)
     else:
         break
+class Patient:
+    def __init__(self, pid, name, disease):
+        self.pid = pid
+        self.name = name
+        self.disease = disease
+
+patients = []
+while True:
+    print("1.Add Patient 2.View Patients 3.Exit")
+    ch = input("Choice: ")
+    if ch == "1":
+        pid = input("ID: ")
+        name = input("Name: ")
+        disease = input("Disease: ")
+        patients.append(Patient(pid, name, disease))
+    elif ch == "2":
+        for p in patients:
+            print(p.pid, p.name, p.disease)
+    else:
+        break
+
