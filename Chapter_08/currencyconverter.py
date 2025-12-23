d7a0d6a05ea09536f1548e1f74e5a28ba64bb32b
Manan <manannagpal003@gmail.com>
2025-12-23 20:35:21 +0530

    created currency converter ex

--- Chapter_08/currencyconverter.py
+++ Chapter_08/currencyconverter.py
@@ -7,3 +7,13 @@ if currency in rates:
     print("Converted:", amt / rates[currency])
 else:
     print("Invalid Currency")
+# Currency Converter: INR to USD/EUR
+# Exchange Rates
+# 1 USD = 83 INR
+# 1 EUR = 90 INR
+# Input Amount in INR
+# Input Target Currency (USD or EUR)
+# Perform Conversion
+# Display Converted Amount or Error Message
+
+
