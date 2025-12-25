404246cd382a2bdbfa6b7839ef382aa715fa5beb
Manan <manannagpal003@gmail.com>
2025-12-25 17:59:25 +0530

    created simple calculator ex

--- Chapter_09/calc.py
+++ Chapter_09/calc.py
@@ -7,3 +7,13 @@ a = int(input("A: "))
 b = int(input("B: "))
 
 print(add(a,b), sub(a,b), mul(a,b), div(a,b))
+# This is a simple calculator that performs addition, subtraction,
+# multiplication, and division on two input numbers.
+# It takes two integers as input and prints the results of the four operations.
+# Example:
+# Input:
+# A: 10
+# B: 5
+# Output:
+# 15 5 50 2.0
+
