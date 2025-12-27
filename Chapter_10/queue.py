f5dd996f3a69a802706c3e50a2803e40847e3d70
Manan <manannagpal003@gmail.com>
2025-12-27 19:38:59 +0530

    created queue implementation

---
+++ Chapter_10/queue.py
@@ -0,0 +1,17 @@
+queue = []
+
+while True:
+    print("1.Enqueue 2.Dequeue 3.Display 4.Exit")
+    ch = input("Choice: ")
+
+    if ch == "1":
+        queue.append(input("Enter element: "))
+    elif ch == "2":
+        if queue:
+            print("Removed:", queue.pop(0))
+        else:
+            print("Queue Empty")
+    elif ch == "3":
+        print(queue)
+    else:
+        break
