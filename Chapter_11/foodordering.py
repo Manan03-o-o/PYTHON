564492c1edb8e015f82c7bd994978560477d3720
Manan <manannagpal003@gmail.com>
2025-12-28 22:19:29 +0530

    created food ordering  system

---
+++ Chapter_11/foodordering.py
@@ -0,0 +1,14 @@
+menu = {"Burger":100, "Pizza":200}
+order = {}
+
+while True:
+    print("\n1.Order 2.Bill 3.Exit")
+    ch = input("Choice: ")
+    if ch == "1":
+        item = input("Item: ")
+        if item in menu:
+            order[item] = menu[item]
+    elif ch == "2":
+        print(order, "Total:", sum(order.values()))
+    else:
+        break
