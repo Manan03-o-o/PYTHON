509e13ad2c0f5969a0eb7039291b8b74c61a7012
Manan <manannagpal003@gmail.com>
2026-01-01 21:30:39 +0530

    created quiz engine2

--- Chapter_13/quiz.py
+++ Chapter_13/quiz.py
@@ -9,3 +9,10 @@ for q,a in qs:
     if time.time()-start<=10 and ans.lower()==a.lower():
         score+=1
 print("Score:",score)
+print("Quiz Over!")
+print("You scored",score,"out of",len(qs))
+with open("quiz_score.txt","a") as f:
+    f.write(f"Score: {score} out of {len(qs)}\n")
+print("Your score has been recorded in quiz_score.txt")
+
+    
