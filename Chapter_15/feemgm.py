a8be9a27cc538e9e5afb8e102a7e53b6fbfb3da5
Manan <manannagpal003@gmail.com>
2026-01-03 20:43:19 +0530

    created complete fee managment systems sample

--- Chapter_15/feemgm.py
+++ Chapter_15/feemgm.py
@@ -9,3 +9,19 @@ while True:
         print(fees)
     else:
         break
+    
+    
+    # Sample Interaction:
+    # 1.Pay Fee 2.View 3.Exit
+    # Choice: 1
+    # Name: Alice
+    # Amount: 1000
+    #
+    # 1.Pay Fee 2.View 3.Exit
+    # Choice: 1
+    # Name: Bob
+    # Amount: 1500
+    # 1.Pay Fee 2.View 3.Exit
+    # Choice: 2
+    # {'Alice': 1000, 'Bob': 1500}
+    
