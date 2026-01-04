b3ca7f48f359414adc80203082503925595e5318
Manan <manannagpal003@gmail.com>
2026-01-04 11:31:46 +0530

    created shop return system

---
+++ Chapter_15/shopreturn.py
@@ -0,0 +1,13 @@
+orders = {"101":"Delivered", "102":"Delivered"}
+
+while True:
+    print("\n1.View Orders 2.Return 3.Exit")
+    ch = input("Choice: ")
+    if ch == "1":
+        print(orders)
+    elif ch == "2":
+        oid = input("Order ID: ")
+        if oid in orders:
+            orders[oid] = "Returned"
+    else:
+        break
