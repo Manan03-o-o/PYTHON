fd00e4c4d12fae34976abcb69ba99d6b99553f83
Manan <manannagpal003@gmail.com>
2026-01-05 20:34:06 +0530

    created bank account  system

---
+++ Chapter_10/bankacc.py
@@ -0,0 +1,15 @@
+balance = 0
+active = True
+
+while active:
+    print("1.Deposit 2.Withdraw 3.Balance 4.Exit")
+    ch = input("Choice: ")
+
+    if ch == "1":
+        balance += int(input("Amount: "))
+    elif ch == "2":
+        balance -= int(input("Amount: "))
+    elif ch == "3":
+        print("Balance:", balance)
+    elif ch == "4":
+        active = False
