f53e77fa959a41be6dd59d5d936962e274cdff2b
Manan <manannagpal003@gmail.com>
2026-01-05 20:33:11 +0530

    created student mgm system

---
+++ Chapter_10/stdmgm.py
@@ -0,0 +1,14 @@
+running = True
+students = {}
+
+while running:
+    print("1.Add 2.View 3.Exit")
+    ch = input("Choice: ")
+
+    if ch == "1":
+        roll = input("Roll: ")
+        students[roll] = input("Name: ")
+    elif ch == "2":
+        print(students)
+    elif ch == "3":
+        running = False
