37a941f67b7631a5324da0707902cbe4e126ad32
Manan <manannagpal003@gmail.com>
2026-01-06 14:35:04 +0530

    created bank transaction system

---
+++ Chapter_05/bank.py
@@ -0,0 +1,17 @@
+balance = 0
+active = True
+
+while active:
+    print("1.Deposit 2.Withdraw 3.Check Balance 4.Exit")
+    ch = input("Choice: ")
+
+    if ch == "1":
+        balance += int(input("Amount: "))
+    elif ch == "2":
+        amt = int(input("Amount: "))
+        if amt <= balance:
+            balance -= amt
+    elif ch == "3":
+        print("Balance:", balance)
+    elif ch == "4":
+        active = False
