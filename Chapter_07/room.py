39acf73d03b26f7a1b649911890d422bf911a4ef
Manan <manannagpal003@gmail.com>
2026-01-07 12:35:12 +0530

    created hotel room booking system

---
+++ Chapter_07/room.py
@@ -0,0 +1,21 @@
+rooms = {101:"Empty", 102:"Empty", 103:"Empty"}
+flag = True
+
+while flag:
+    print("1.Book 2.Checkout 3.Status 4.Exit")
+    ch = input("Choice: ")
+
+    if ch == "1":
+        r = int(input("Room: "))
+        if rooms.get(r) == "Empty":
+            rooms[r] = input("Guest Name: ")
+
+    elif ch == "2":
+        r = int(input("Room: "))
+        rooms[r] = "Empty"
+
+    elif ch == "3":
+        print(rooms)
+
+    elif ch == "4":
+        flag = False
