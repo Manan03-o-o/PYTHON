a17e9cd84a5057a64ebe8f835f6d4e38752b36d4
Manan <manannagpal003@gmail.com>
2026-01-07 12:32:29 +0530

    created bank system with transaction history

---
+++ Chapter_07/bank.py
@@ -0,0 +1,25 @@
+balance = 0
+history = []
+active = True
+
+while active:
+    print("1.Deposit 2.Withdraw 3.History 4.Exit")
+    ch = input("Choice: ")
+
+    if ch == "1":
+        amt = int(input("Amount: "))
+        balance += amt
+        history.append(("Deposit", amt))
+
+    elif ch == "2":
+        amt = int(input("Amount: "))
+        if amt <= balance:
+            balance -= amt
+            history.append(("Withdraw", amt))
+
+    elif ch == "3":
+        print("Balance:", balance)
+        print("History:", history)
+
+    elif ch == "4":
+        active = False
